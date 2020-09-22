377abad1609f43f9f2dea1e2d1a9df2cd6dc2fd9
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2020-09-21 20:21:41 -0400

    writing prompts

--- script.js
+++ script.js
@@ -10,5 +10,15 @@ function writePassword() {
 
 }
 
+prompt("How many characters do you want your password to be?(must be between 8 and 120")
+
+prompt("Do you want your password to include lowercase letters?")
+
+prompt("Do you want your password to include UPPERCASE letters?")
+
+prompt("Do you want your password to contain numbers?")
+
+prompt("Do you want your password to contain special characters?")
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
