069cdaae2fd03430bdb976898a393cd72d5dff27
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2020-09-22 12:41:36 -0400

    edits

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCase = ["a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z"];
+var upperCase = ["A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var characters = [!, @, #, %, &, ?];
 
 // Write password to the #password input
 function writePassword() {
@@ -9,16 +13,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-prompt("How many characters do you want your password to be?(must be between 8 and 120")
-
-prompt("Do you want your password to include lowercase letters?")
-
-prompt("Do you want your password to include UPPERCASE letters?")
-
-prompt("Do you want your password to contain numbers?")
-
-prompt("Do you want your password to contain special characters?")
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
